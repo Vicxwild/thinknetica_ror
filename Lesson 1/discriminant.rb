59f0e1307104578da46e14d51b5c28a24ebfe196
Vicxwild <vicxwild@gmail.com>
2022-06-23 18:56:43 +0300

    Lesson 1

--- Lesson 1/discriminant.rb
+++ Lesson 1/discriminant.rb
@@ -8,8 +8,8 @@ c = Integer(gets.chomp)
 d = b ** 2 - 4 * a * c
 
 if d > 0
-	x_one = (-b + c) / (2 * a)
-	x_two = (-b - c) / (2 * a)
+	x_one = (-b + d ** 0.5) / (2 * a)
+	x_two = (-b - d ** 0.5) / (2 * a)
 
 	puts "d = #{d}, x1 = #{x_one}, x2 = #{x_two}"
 elsif d == 0
