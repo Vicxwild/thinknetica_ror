251424bfc5c2b71346d5dc1381cd8d30a7266a74
Vicxwild <vicxwild@gmail.com>
2022-06-23 18:51:07 +0300

    Lesson 1

---
+++ Lesson 1/discriminant.rb
@@ -0,0 +1,20 @@
+puts "a = "
+a = Integer(gets.chomp)
+puts "b = "
+b = Integer(gets.chomp)
+puts "c = "
+c = Integer(gets.chomp)
+
+d = b ** 2 - 4 * a * c
+
+if d > 0
+	x_one = (-b + c) / (2 * a)
+	x_two = (-b - c) / (2 * a)
+
+	puts "d = #{d}, x1 = #{x_one}, x2 = #{x_two}"
+elsif d == 0
+	x = -b / (2 * a)
+	puts "d = #{d}, x = #{x}"
+else 
+	puts "d = #{d}, корней нет!"
+end
--- Lesson 1/right_triangle.rb
+++ Lesson 1/right_triangle.rb
@@ -6,11 +6,25 @@ puts "The length of the third side is: "
 c_side = Integer(gets.chomp)
 
 if a_side > b_side && a_side > c_side
-	puts "Triangle is a right" if a_side ** 2 == b_side ** 2 + c_side ** 2
+	hupotenuse = a_side
+	cathet_1 = b_side
+	cathet_2 = c_side
 elsif b_side > a_side && b_side > c_side
-	puts "Triangle is a right" if b_side ** 2 == a_side ** 2 + c_side ** 2
+	hupotenuse = b_side
+	cathet_1 = a_side
+	cathet_2 = c_side
 elsif c_side > a_side && c_side > b_side
-	puts "Triangle is a right" if c_side ** 2 == a_side ** 2 + b_side ** 2
-else
-	puts "krivo!!!!!!!"
+	hupotenuse = c_side
+	cathet_1 = a_side
+	cathet_2 = b_side
+end
+
+if a_side == b_side && b_side == c_side
+	puts "The triangle is equilateral"
+elsif a_side == b_side || b_side == c_side || c_side == a_side
+	puts "The triangle is isosceles"
+elsif hupotenuse ** 2 == (cathet_1 ** 2 + cathet_2 ** 2)
+	puts "The triangle is rectangulare"
+else 
+	puts "An ordinary triangle"
 end
